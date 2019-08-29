6c9c2f8f888ae0aa6927c5f3ac0243f2bd2d8ea3
heptacode <admin@hyunwoo.kim>
2019-08-29 15:27:28 +0900

    Create Sruser Model

--- ch2/board/models.py
+++ ch2/board/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Sruser(models.Model):
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    registerd = models.DateTimeField(auto_now_add=True)
