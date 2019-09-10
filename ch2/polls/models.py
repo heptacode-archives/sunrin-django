7027f893c1cf92a53613166422efc922a1c09c94
heptacode <admin@hyunwoo.kim>
2019-09-10 14:07:27 +0900

    Create Polls

--- ch2/polls/models.py
+++ ch2/polls/models.py
@@ -10,9 +10,9 @@ class Question(models.Model):
         return self.question_text
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=200) # 답변 항목 문구
+    votes       = models.IntegerField(default=0) # 투표 카운트
+    question    = models.ForeignKey(Question, on_delete=models.CASCADE) # Foreign Key
 
     def __str__(self):
         return self.choice_text
--- second/poll/models.py
+++ second/poll/models.py
@@ -7,3 +7,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200) # 답변 항목 문구
+    votes       = models.IntegerField(default=0) # 투표 카운트
+    question    = models.ForeignKey(Question, on_delete=models.CASCADE) # Foreign Key
+
+    def __str__(self):
+        return self.choice_text
