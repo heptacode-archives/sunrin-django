b3ca24b28e3ddb3ae8bd7340bbbc79ee5de4e619
heptacode <admin@hyunwoo.kim>
2019-09-26 15:22:52 +0900

    Add detail.html

--- second/poll/views.py
+++ second/poll/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from poll.models import Question
 
 # Create your views here.
-def index :
+def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = { 'latest_question_list': latest_question_list }
     return render(request, 'index.html', context)
